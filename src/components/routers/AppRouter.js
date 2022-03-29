125ed26b7be7324589b9bf01c31338562d846cf9
gugohh <oohhgug@gmail.com>
2022-03-29 19:36:52 +0200

    second part of router done

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,22 +1,15 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-import { Navbar } from "../nav/NavBar";
-
-import DcScreen from "../dc/DcScreen";
 import LoginScreen from "../login/LoginScreen";
-import MarvelScreen from "../marvel/MarvelScreen";
-import SearchScreen from "../search/SearchScreen";
+import DashboardRoutes from "./DashboardRoutes";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route exact path="/dc" element={<DcScreen />} />
-        <Route exact path="/marvel" element={<MarvelScreen />} />
-        <Route exact path="/search" element={<SearchScreen />} />
-        <Route exact path="/login" element={<LoginScreen />} />
+        <Route  path="/login" element={<LoginScreen />} />
+        <Route  path="/*"  element={<DashboardRoutes/>}/>
       </Routes>
     </BrowserRouter>
   );
