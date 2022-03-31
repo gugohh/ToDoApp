bccae5d43a7a867165be8bde85a530663fb16021
gugohh <oohhgug@gmail.com>
2022-03-31 20:58:49 +0200

    animation with  animate.css

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -18,14 +18,15 @@ const HeroScreen = () => {
   const handleReturn = () => {
     navigate(-1);
   };
+  
   const imgPath = `/assets/${id}.jpg`;
 
   return (
     <div className="row mt-5">
       <div className="col-4">
-        <img src={imgPath} alt={hero.superhero} className="img-thumbnail" />
+        <img src={imgPath} alt={superhero} className="img-thumbnail animate__animated animate__fadeInLeft" />
       </div>
-      <div className="col-8">
+      <div className="col-8 animate__animated animate__fadeIn">
         <h3>{hero.superhero}</h3>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">
